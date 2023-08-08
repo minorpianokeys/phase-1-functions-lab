f0d241c7a3258813172318dff5e39f33a1c6df89
Michael Friedman <minorpianokeys@gmail.com>
2023-08-08 10:41:03 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    let numberOfBlocks = Math.abs(location - 42);
+    return numberOfBlocks;
+}
+
+function distanceFromHqInFeet(location) {
+    let numberOfBlocks = distanceFromHqInBlocks(location);
+    let feet = numberOfBlocks * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let feetTravelled = Math.abs(start - destination) * 264;
+    return feetTravelled;
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTravelled = distanceTravelledInFeet(start, destination);
+    if (feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled <= 2000) {
+        return (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
